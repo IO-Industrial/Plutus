e14e08da1c530f45ce515626c962b72955562d4e
Joe Turner <joe@agavemountain.com>
2023-03-21 21:31:15 -0600

    Add hello world
    Added hello world Wt application.

---
+++ erp-server/src/main.cpp
@@ -0,0 +1,59 @@
+//! \brief Web application program entry point
+//! \file main.cpp
+//!
+//! \copyright
+//!
+//! This file is part of the ERPBeyond distribution.
+//! Copyright (c) 2023 Joe Turner.
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, version 3.
+//!
+//! This program is distributed in the hope that it will be useful, but
+//! WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//! General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program. If not, see <http://www.gnu.org/licenses/>.
+#include <Wt/WApplication.h>
+#include <Wt/WBreak.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WLineEdit.h>
+#include <Wt/WPushButton.h>
+#include <Wt/WText.h>
+#include <memory>
+
+class HelloApplication : public Wt::WApplication
+{
+public:
+    HelloApplication(const Wt::WEnvironment& env);
+
+private:
+    Wt::WLineEdit *nameEdit_;
+    Wt::WText *greeting_;
+};
+
+HelloApplication::HelloApplication(const Wt::WEnvironment& env)
+    : Wt::WApplication(env)
+{
+    setTitle("Hello world");
+
+    root()->addWidget(std::make_unique<Wt::WText>("Your name, please? "));
+    nameEdit_ = root()->addWidget(std::make_unique<Wt::WLineEdit>());
+    Wt::WPushButton *button = root()->addWidget(std::make_unique<Wt::WPushButton>("Greet me."));
+    root()->addWidget(std::make_unique<Wt::WBreak>());
+    greeting_ = root()->addWidget(std::make_unique<Wt::WText>());
+    auto greet = [this]{
+      greeting_->setText("Hello there, " + nameEdit_->text());
+    };
+    button->clicked().connect(greet);
+}
+
+int main(int argc, char **argv)
+{
+    return Wt::WRun(argc, argv, [](const Wt::WEnvironment& env) {
+      return std::make_unique<HelloApplication>(env);
+    });
+}
